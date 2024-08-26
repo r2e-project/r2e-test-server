f09994fc0d5739637983ed7390eb2226c6f3dd1c
manishshettym <manishs@berkeley.edu>
2024-08-26 10:44:54 -0700

    chore: black format

--- r2e_test_server/server.py
+++ r2e_test_server/server.py
@@ -87,7 +87,10 @@ class R2EService(rpyc.Service):
         except Exception as e:
             traceback_message = traceback.format_exc()
             output = stdout_buffer.getvalue().strip()
-            return {"error": f"Error: {traceback_message}\n\nSmall Error: {repr(e)}", "output": output}
+            return {
+                "error": f"Error: {traceback_message}\n\nSmall Error: {repr(e)}",
+                "output": output,
+            }
 
     @rpyc.exposed
     def submit(self):
